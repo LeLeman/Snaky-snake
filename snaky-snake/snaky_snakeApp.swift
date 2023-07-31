953bfe8d1b565b89b38252e408fbd3fc5dcf95f1
Eugene Lemish <bandapandaaa@gmail.com>
2023-07-31 16:22:30 +0300

    Initial Commit

---
+++ snaky-snake/snaky_snakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  snaky_snakeApp.swift
+//  snaky-snake
+//
+//  Created by Evgeniy Lemish on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct snaky_snakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
